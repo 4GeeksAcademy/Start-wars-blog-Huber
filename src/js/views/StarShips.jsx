df2759a7fbe3922bb989961ef0c24ce23733478d
Huber Soto <104796746+hubers0@users.noreply.github.com>
2024-07-03 20:36:59 +0000

    first

--- src/js/views/StarShips.jsx
+++ src/js/views/StarShips.jsx
@@ -64,7 +64,7 @@ export const Starship = () => {
                     </div>
                 </div>
             ) : (
-                <p>Loading planet...</p>
+                <p>Loading Starships...</p>
             )}
             </div>
         </React.Fragment>
